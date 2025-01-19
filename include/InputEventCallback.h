b15d0f44c80ad65f297da407a93de27641f2bc5f
Chen Jiaxin <chenjiaxin@shopee.com>
2025-01-19 21:31:30 +0800

    termux display client init

--- include/InputEventCallback.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include "InputEvent.h"
-#include "SocketIPCClient.h"
-class InputEventCallback{
-public:
-    virtual void callback(InputEvent ev);
-};
--- renderer/InputEventCallback.cpp
+++
@@ -1,6 +0,0 @@
-#include "InputEventCallback.h"
-#include "LogUtil.h"
-
-void InputEventCallback::callback(InputEvent ev) {
-//    LOG_I("%hhu", ev.type);
-}
