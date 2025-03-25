d24f653520ee6c4e4e235383666ee27f5ac2bb16
Chen Jiaxin <chenjiaxin@shopee.com>
2025-03-25 20:57:16 +0800

    termux display client

--- renderer/termuxdc_server.cpp
+++ renderer/termuxdc_server.cpp
@@ -125,6 +125,7 @@ ssize_t termuxdc_server::recv_event(int sockfd, void *buffer, size_t length) {
     return total_received;
 }
 int termuxdc_server::waitEvent(termuxdc_event *event) {
+    printf("<======waitEvent========>");
     if (dataSocket > 0) {
         ssize_t result = recv_event(dataSocket, event, sizeof(*event));
         if (result < 0) {
